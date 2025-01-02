1e24db5f9de74ee8dfe36d3d07ce9f3fc37fd85c
rustleupchef <rustle.up.chef@gmail.com>
2025-01-01 23:25:21 -0600

    fixed python code

--- QSS/python/main.py
+++ QSS/python/main.py
@@ -1,8 +1,8 @@
-from dotenv import load_dotenv
-load_dotenv()
+from dotenv import load_dotenv,find_dotenv
+load_dotenv(find_dotenv())
 
 def main() -> None:
     pass # write code here
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     main()
